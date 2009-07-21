9ddb9e23e832cc72ea93a2c3387294a0543cadb9
Jose Soares <jsoares@int1665.local>
2009-07-21 10:48:48 -0400

    Fixed urls

--- slots/urls.py
+++ slots/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('slots.views',
-    (r'^add/(?P<slotname>[\w_]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'add'),
-    (r'^remove/(?P<slotname>[\w_]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'remove'),
+    (r'^add/(?P<slotname>[\w_-]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'add'),
+    (r'^remove/(?P<slotname>[\w_-]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'remove'),
 )
