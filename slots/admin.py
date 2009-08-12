b489c9baf26063dd9f247e0b6f04034f2af39fdf
Jose Soares <jsoares@nei1220.nwc.newsworld.net>
2009-08-12 14:22:25 -0400

    Added slot content order managment

--- slots/admin.py
+++ slots/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from slots.models import Slot
+from slots.models import Slot, SlotContent
 
 class SlotAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'is_static', 'count', )
     search_fieldsets = ('name', 'description', )
     list_per_page = 25
     filter_horizontal = ('eligible_types','sites',)
-    
+
+
+class SlotContentAdmin(admin.ModelAdmin):
+    list_display = ('slot', 'content_object', 'order', 'add_date', )
+    list_per_page = 25
+
 
 admin.site.register(Slot, SlotAdmin)
+admin.site.register(SlotContent, SlotContentAdmin)
