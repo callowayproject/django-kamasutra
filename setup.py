677596e3e0c64894282a37624b8619cd7043bdc2
Jose Soares <jsoares@int1665.local>
2010-01-20 17:37:27 -0500

    More name change fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
-setup(name='django-slots',
+setup(name='django-kamasutra',
       version='.1',
-      description='The need to put something, somewhere',
+      description='',
       long_description='',
       author='Jose Soares',
       author_email='jsoares@washingtontimes.com',
-      url='http://opensource.washingtontimes.com/projects/django-slots/',
-      packages=['slots'],
+      url='http://opensource.washingtontimes.com/projects/django-kamasutra/',
+      packages=['positions'],
       classifiers=['Framework :: Django',
           'License :: OSI Approved :: Apache Software License',
           'Development Status :: 4 - Beta',
