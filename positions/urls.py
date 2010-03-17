cda96d7894879fb8409859e5f4d037a7d0dd8a21
Jose Soares <jsoares@int1665.local>
2010-03-17 10:26:46 -0400

    Fixed spelling mistake in urls

--- positions/urls.py
+++ positions/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('position.views',
+urlpatterns = patterns('positions.views',
     (r'^add/(?P<position_name>[\w_-]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'add'),
     (r'^remove/(?P<position_name>[\w_-]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'remove'),
     (r'^(?P<position_id>.*)/order_content/$', 'order_content'),
