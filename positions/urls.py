5bce13c6a77e15ae4d08deea8f7d36c0deebb75b
Jose Soares <jsoares@adv1179.nwc.newsworld.net>
2010-03-18 10:11:51 -0400

    Added url for widget data

--- positions/urls.py
+++ positions/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('positions.views',
     (r'^add/(?P<position_name>[\w_-]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'add'),
     (r'^remove/(?P<position_name>[\w_-]+)/(?P<type>[\d]+)/(?P<id>[\d]+)/$', 'remove'),
     (r'^(?P<position_id>.*)/order_content/$', 'order_content'),
+    (r'^widget/data/(?P<content_type_id>.*)/(?P<object_id>.*)/', 'widget_data')
 )
