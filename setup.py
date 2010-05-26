5da47f61942464d03cd340e3b3017da6b8802127
Jose Soares <jsoares@int1665.nwc.newsworld.net>
2010-05-26 12:16:01 -0400

    Changed version to 0.1.3 and added admin url fallback

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kamasutra',
-      version='0.1.2',
+      version='0.1.3',
       description='',
       long_description='',
       author='Jose Soares',
