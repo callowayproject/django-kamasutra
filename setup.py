f0e19bc645f1049d71866d64802a52eb7b50ff7a
Jose Soares <jsoares@int1665.nwc.newsworld.net>
2010-05-27 14:55:05 -0400

    Bumped version to 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kamasutra',
-      version='0.1.5',
+      version='0.1.6',
       description='',
       long_description='',
       author='Jose Soares',
