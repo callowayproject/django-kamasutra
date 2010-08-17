df44783a9fff9a6fce2ac166eeb4b8fcb7d471cb
Jose Soares <jsoares@int1665.nwc.newsworld.net>
2010-08-17 11:34:26 -0400

    Cleaned up the settings, removed the cache settings and template debug

--- positions/settings.py
+++ positions/settings.py
@@ -1,16 +1,11 @@
 from django.conf import settings
 
-# Cache timeout for all position content
-CACHE_TIMEOUT = getattr(settings, 'POSITION_CACHE_TIMEOUT', 60)
-# Cache prefix for all postion keys
-CACHE_PREFIX = getattr(settings, 'POSITION_CACHE_PREFIX', 'position')
-# The amout of items to overlap the position.count variable
+# The amout of items to overlap the position.count
 CONTENT_OVERLAP_COUNT = getattr(settings, 'POSITION_CONTENT_OVERLAP_COUNT', 2)
-# String used to combine when retrieving a position via templatetags
-CONBINE_STRING = getattr(settings, 'POSITION_PAGE_COMBINE_STRING', '__')
 
-ADMIN_URL = getattr(settings, 'POSITION_ADMIN_URL', 'admin')
+# String used to combine when retrieving a position via templatetags
+CONBINE_STRING = getattr(settings, 'POSITION_COMBINE_STRING', '__')
 
 # List of templates per model
+# EX: {"stories.story": "customtemplates/stories/position_render.html"}
 TEMPLATES = getattr(settings, "POSITION_TEMPLATES", {})
-
