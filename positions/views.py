11882bde6419071c9be2f21884d14884553b70b6
Jose Soares <jsoares@int1665.nwc.newsworld.net>
2010-08-17 11:33:34 -0400

    Fixed up the views some, removed purge_cache, since caching is no longer apart of positions. Added reverse for some urls

--- positions/views.py
+++ positions/views.py
@@ -3,13 +3,16 @@ from django.views.decorators.cache import cache_page, never_cache
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.utils.safestring import mark_safe
-from django.core import urlresolvers
+from django.core.urlresolvers import reverse
 from django.core.cache import cache
 
-import simplejson
+try:
+    import simplejson
+except ImportError:
+    simplejson = None
 
 from positions.models import Position, PositionContent
 from positions.forms import PositionContentOrderForm
@@ -22,14 +25,19 @@ def get_admin_url(obj, fallback="/admin/positions/"):
     performed.
     """
     if hasattr(obj, "_meta"):
-        return "/%s/%s/%s/%s" % (position_settings.ADMIN_URL, obj._meta.app_label,
+        return "/%s/%s/%s/%s" % (reverse("admin:index"), obj._meta.app_label,
             obj._meta.module_name, obj.id)
     return fallback
  
-
-def widget_data(request, content_type_id, object_id):
-    c = ContentType.objects.get(id=content_type_id)
-    obj = c.get_object_for_this_type(id=object_id)
+def json_data(request, content_type_id, object_id):
+    """
+    This is used for the admin widget
+    """
+    if not simplejson:
+        return Http404
+        
+    ctype = get_object_or_404(ContentType, pk=content_type_id)
+    obj = ctype.get_object_for_this_type(id=object_id)
     
     positions = Position.objects.get_applicable(obj)
     
@@ -40,13 +48,11 @@ def widget_data(request, content_type_id, object_id):
     
  
 def add(request, position_name, type, id):
-    try:
-        position = Position.objects.get(name__iexact=position_name)
-    except Position.DoesNotExist:
-        raise Http404
-        
-    c = ContentType.objects.get(id=type)
-    obj = c.get_object_for_this_type(id=id)
+    
+    position = get_object_or_404(Position, name__iexact=position_name)
+    ctype = get_object_or_404(ContentType, pk=type)
+    
+    obj = ctype.get_object_for_this_type(id=id)
     if position not in Position.objects.get_applicable(obj):
         return HttpResponseForbidden("The object cannot be added to this position.")
     
@@ -55,20 +61,16 @@ def add(request, position_name, type, id):
     return HttpResponseRedirect(get_admin_url(obj))
 add = staff_member_required(add)
 
-
 def remove(request, position_name, type, id):
     """
     Resolves the object by the specified content type and id, and removes it
     from the specified Position if it has already been added.
     """
     next = request.GET.get('next', '')
-    try:
-        position = Position.objects.get(name__iexact=position_name)
-    except Position.DoesNotExist:
-        raise Http404
+    position = get_object_or_404(Position, name__iexact=position_name)
         
-    c = ContentType.objects.get(id=type)
-    obj = c.get_object_for_this_type(id=id)
+    ctype = get_object_or_404(ContentType, pk=type)
+    obj = ctype.get_object_for_this_type(id=id)
     
     Position.objects.remove_object(position=position, obj=obj)
     
@@ -80,15 +82,12 @@ remove = staff_member_required(remove)
 
 def order_content(request, position_id, template_name='admin/positions/order.html'):
     
-    try:
-        position = Position.objects.get(pk=position_id)
-    except Position.DoesNotExist:
-        raise Http404
+    position = get_object_or_404(Position, pk=position_id)
         
     forms = []
     if request.method == 'POST':
         if 'cancel' in request.POST:
-            return HttpResponseRedirect('/%s/positions/position/' % position_settings.ADMIN_URL)
+            return HttpResponseRedirect('/%s/positions/position/' % reverse("admin:index"))
         for content in position.positioncontent_set.all():
             form = PositionContentOrderForm(request.POST, instance=content, prefix=str(content.pk))
             forms.append(form)
@@ -107,7 +106,7 @@ def order_content(request, position_id, template_name='admin/positions/order.htm
             form.fields['order'].content_edit = ""
             if content.content_object and hasattr(content.content_object, "_meta"):
                 admin_url_name = "admin:%s_%s_change" % (content.content_object._meta.app_label, content.content_object._meta.module_name)
-                form.fields['order'].content_edit = urlresolvers.reverse(admin_url_name, args=(content.content_object.pk,))
+                form.fields['order'].content_edit = reverse(admin_url_name, args=(content.content_object.pk,))
             forms.append(form)
     
     return render_to_response(template_name, 
@@ -116,16 +115,6 @@ def order_content(request, position_id, template_name='admin/positions/order.htm
                                  context_instance=RequestContext(request))      
 order_content = staff_member_required(order_content)
 order_content = never_cache(order_content)
-
-def purge_cache(request, position_id, template_name='admin/positions/purge_cache.html'):
-    
-    position = get_object_or_404(Position, pk=position_id)
-        
-    PositionContent.objects.rebuild_cache(position)
-    
-    return render_to_response(template_name,
-                              {'position': position},
-                              context_instance=RequestContext(request))
     
     
     
